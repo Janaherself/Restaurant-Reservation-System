5de91843bd2535d9ae632ba3a29b8f46e995b0d2
Jana Saa <jana.abusaa@gmail.com>
2024-10-15 23:14:56 +0300

    Seed Data

--- RestaurantReservation.Db/DataModels/Order.cs
+++ RestaurantReservation.Db/DataModels/Order.cs
@@ -2,10 +2,18 @@
 {
     public class Order
     {
+        public Order()
+        {
+            OrderItems = new List<OrderItem>();
+        }
+
         public int  OrderId { get; set; }
-        public int ReservationId { get; set; }
-        public int EmployeeId { get; set; }
+        public int? ReservationId { get; set; }
+        public int? EmployeeId { get; set; }
         public DateTime OrderDate { get; set; }
         public int TotalAmount { get; set; }
+        public Reservation Reservation { get; set; }
+        public Employee Employee { get; set; }
+        public List<OrderItem> OrderItems { get; set; } 
     }
 }
--- RestaurantReservation.Db/DataModels/Reservation.cs
+++ RestaurantReservation.Db/DataModels/Reservation.cs
@@ -2,11 +2,20 @@
 {
     public class Reservation
     {
+        public Reservation()
+        {
+            Orders = new List<Order>();
+        }
+
         public int ReservationId { get; set; }
-        public int CustomerId { get; set; }
-        public int RestaurantId { get; set; }
-        public string TableId { get; set; }
+        public int? CustomerId { get; set; }
+        public int? RestaurantId { get; set; }
+        public int? TableId { get; set; }
         public DateTime ReservationDate { get; set; }
         public int PartySize { get; set; }
+        public Customer Customer { get; set; }
+        public Restaurant Restaurant { get; set; }
+        public Table Table { get; set; }
+        public List<Order> Orders { get; set; }
     }
 }
